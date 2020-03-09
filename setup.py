ce5985c02536784e5e9c6a8562940efa17652a26
Remo Giermann <mo@liberejo.de>
2020-03-09 18:19:56 +0100

    added setup.py for installation

---
+++ setup.py
@@ -0,0 +1,31 @@
+import re
+from setuptools import setup
+
+version = re.search(
+    r'^VERSION\s=\s"(.*)"',
+    open('spell/spell.py').read(),
+    re.M
+    ).group(1)
+
+name = re.search(
+    r'^NAME\s=\s"(.*)"',
+    open('spell/spell.py').read(),
+    re.M
+    ).group(1)
+
+setup(
+    name = name,
+    version = version,
+    packages = [name],
+    package_data = {
+        "spell": ["tables/*.yml"]
+    },
+    entry_points = {
+        "console_scripts": ["spell=spell.spell:main"]
+    },
+    description = "Spell text using a spelling alphabet",
+    long_description = "Commandline utility to spell any given text using different spelling alphabets.",
+    author = "Remo Giermann",
+    author_email = "mo@liberejo.de",
+)
+
